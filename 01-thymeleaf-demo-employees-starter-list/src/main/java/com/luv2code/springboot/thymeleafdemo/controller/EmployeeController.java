8fe0a4e2dab18c085e13546f65e890b9bdf2c75e
Marcin <gasiorek.mail@gmail.com>
2023-08-04 11:31:33 +0200

    implemented save Employee form -adds new emplpyee to database

--- 01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -38,6 +40,11 @@ public class EmployeeController {
         model.addAttribute("employee", employee);
 
         return "employees/employee-form";
+    }
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee")Employee employee){
+        employeeService.save(employee);
+        return "redirect:/employees/list";
     }
 }
 
