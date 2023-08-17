c499b57c534deede9cb46a41e8a697cdd3c3182b
Marcin <gasiorek.mail@gmail.com>
2023-08-17 07:25:03 +0200

    add AOP Logging support

---
+++ 01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -0,0 +1,56 @@
+package com.luv2code.springboot.thymeleafdemo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class DemoLoggingAspect {
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.controller.*.*(..))")
+    private void forControllerPackage() {
+
+    }
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.service.*.*(..))")
+    private void forServicePackage() {
+
+    }
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.dao.*.*(..))")
+    private void forDaoPackage() {
+
+    }
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forApplicationFlow() {
+
+    }
+
+    @Before("forApplicationFlow()")
+    public void before(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().toShortString();
+        myLogger.info(" in @Before calling method: " + method);
+
+        Object[] args = joinPoint.getArgs();
+        for (Object tempArg : args) {
+            myLogger.info("====> argument: " + tempArg);
+        }
+    }
+
+    @AfterReturning(
+            pointcut = "forApplicationFlow()",
+            returning = "result")
+    public void afterReturning(JoinPoint joinPoint, Object result) {
+        String method = joinPoint.getSignature().toShortString();
+        myLogger.info(" in @AfterReturning calling method: " + method);
+        myLogger.info("=====> result: " + result);
+    }
+}
